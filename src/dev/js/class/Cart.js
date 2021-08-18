8a41c56570571e7f407a4dc0a7357cae7f23b7f9
Стас Миронов <stasmironovweb@gmail.com>
2021-08-19 01:05:26 +0300

    app

---
+++ src/dev/js/class/Cart.js
@@ -0,0 +1,116 @@
+class Cart {
+  constructor(
+    items,
+    cartClass = "cart",
+    plusClass = "js-plus",
+    minusClass = "js-minus",
+    deleteClass = "delete",
+    currency = ""
+  ) {
+    this.items = items;
+    this.plusClass = plusClass;
+    this.minusClass = minusClass;
+    this.deleteClass = deleteClass;
+    this.cartClass = cartClass;
+    this.currency = "₽";
+  }
+  goodsPlus(art) {
+    this.items[art]["count"]++;
+  }
+  goodsMinus(art) {
+    if (this.items[art]["count"] - 1 <= 0) {
+      this.goodsDelete(art);
+    } else {
+      this.items[art]["count"]--;
+    }
+  }
+  goodsDelete(art) {
+    delete this.items[art];
+  }
+  getTotal() {
+    let total = 0;
+    for (let key in this.items) {
+      total += this.items[key]["count"] * this.items[key]["price"];
+    }
+    return total;
+  }
+  render() {
+    let out = "";
+    for (let key in this.items) {
+      out += `<div class="cart-card js-card">`;
+
+      out += `<div class="cart-card__column cart-card__column--img">`;
+      out += `<img src="${this.items[key]["image"]}">`;
+      // out += `<button class="to-cart button-primary" data-articul="${key}">В корзину</button>`;
+      out += `</div>`;
+
+      out += `<div class="cart-card__column cart-card__column--title">`;
+      out += `${this.items[key]["name"]}`;
+      out += `</div>`;
+
+      out += `<div class="cart-card__column cart-card__column--size">`;
+      out += `<div class="qty-bloc">`;
+      out += `<div class="qty-bloc__txt">Размер:</div>`;
+      out += `<select class="js-select">`;
+      out += `<option value="S">S</option>`;
+      out += `<option value="M">M</option>`;
+      out += `<option value="L">L</option>`;
+      out += `</select>`;
+      out += `</div>`;
+      out += `</div>`;
+
+      out += `<div class="cart-card__column cart-card__column--qty">`;
+      out += `<div class="quantity">`;
+
+      out += `<div class="quantity__txt">Кол-во:</div>`;
+
+      out += `<div class="quantity__box">`;
+      out += `<button class="minus js-minus" data-articul="${key}"><svg class="b-svg svg-minus"><use xlink:href="svg/symbol/sprite.svg#minus"></use></svg></button>`;
+      out += `<input class="quantity__field js-input-qty" type="text" data-min="1" value=${this.items[key]["count"]} data-step="1">`;
+      out += `<button class="plus js-plus" data-articul="${key}"><svg class="b-svg svg-plus"><use xlink:href="svg/symbol/sprite.svg#plus"></use></svg></button>`;
+      out += `</div>`;
+
+      out += `<div class="quantity__cost">`;
+      out += `<span class="cost js-cost">${this.items[key]["price"]}</span>`;
+      out += `<span class="symbol">₽/шт.</span>`;
+      out += `</div>`;
+
+      out += `</div>`;
+      out += `</div>`;
+
+      out += `<div class="cart-card__column cart-card__column--favourites">`;
+      out += `<label class="favorite">`;
+      out += `<input type="checkbox">`;
+      out += `<span class="favorite__icon js-favorite"><svg width="20" height="22" viewBox="-1 0 28 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M17.9 0C15.4 0 13.3 1.4 12.2 3.5C11.1 1.4 9 0 6.5 0C2.9 0 0 3 0 6.7C0 7.8 0.3 8.9 0.8 9.8C0.8 9.9 1 10.2 1.3 10.6C1.5 10.9 1.7 11.1 2 11.4C4.9 14.8 12.3 22 12.3 22C12.3 22 19.7 14.8 22.6 11.4C22.8 11.2 23.1 10.9 23.3 10.6C23.6 10.2 23.8 9.9 23.8 9.8C24.3 8.9 24.6 7.8 24.6 6.7C24.4 3 21.5 0 17.9 0Z" fill="#FF6C96"></path>
+        </svg></span>`;
+      out += `<span class="favorite__txt">В избранное</span>`;
+      out += `</label>`;
+      out += `</div>`;
+
+      out += `<div class="cart-card__column cart-card__column--price">`;
+      out += `<span class="cost">${
+        this.items[key]["price"] * this.items[key]["count"]
+      }</span>`;
+      out += `<span class="symbol">₽</span>`;
+      out += `</div>`;
+
+      out += `<div class="cart-card__column cart-card__column--del">`;
+      out += `<div class="delete" data-articul="${key}">`;
+      out += `<div class="delete__txt"> Удалить`;
+      out += `</div>`;
+      out += `</div>`;
+      out += `</div>`;
+
+      out += `</div>`;
+    }
+
+    $(".js-itog").text(this.getTotal());
+    $(".js-cart-qty").text(`(${Object.keys(this.items).length})`);
+    $(".js-cart-dis").text(this.getTotal());
+
+    return out;
+  }
+}
+
+export default Cart;
---
+++ src/dev/js/main.js
@@ -0,0 +1,146 @@
+import Slider from "./class/Slider.js";
+import Cart from "./class/Cart.js";
+import $ from "jquery";
+import "select2";
+
+$.fn.exists = function () {
+  return $(this).length;
+};
+
+const projectFunc = {
+  initSelect(elem) {
+    if ($(elem).exists()) {
+      try {
+        $(elem).select2();
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  },
+  initElect() {
+    if ($(".js-favorite ").exists()) {
+      try {
+        $(".js-favorite").each((_, item) => {
+          $(item).on("click", () => {
+            $(item).toggleClass("open");
+            if ($(item).find("input").is(":checked")) {
+              $(item).find("input").prop("checked", false);
+            }
+          });
+        });
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  },
+  calc(elem, status) {
+    if (status) {
+      elem.val(+elem.val() + 1);
+    } else {
+      if (+elem.val() > 0) {
+        elem.val(+elem.val() - 1);
+      }
+    }
+  },
+  numberWithSpaces(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+  },
+  createSlider() {
+    const slider = new Slider(".js-slider", 6.37, 18);
+    slider.createSlider();
+
+    $(window)
+      .on("resize load", function () {
+        if ($(window).width() <= 1300 && $(window).width() > 1235) {
+          slider.updateSlider("space", 13);
+          slider.updateSlider("view", 5);
+        } else if ($(window).width() <= 1235 && $(window).width() > 1024) {
+          slider.updateSlider("view", 4.6);
+        } else if ($(window).width() <= 1024 && $(window).width() > 767) {
+          slider.updateSlider("view", 4);
+        } else if ($(window).width() <= 767 && $(window).width() > 620) {
+          slider.updateSlider("view", 3.3);
+        } else if ($(window).width() <= 620 && $(window).width() > 320) {
+          slider.updateSlider("view", 2.35);
+        } else {
+          slider.updateSlider("space", 18);
+          slider.updateSlider("view", 6.37);
+        }
+      })
+      .resize();
+  },
+};
+
+function init() {
+  projectFunc.createSlider();
+}
+
+$(() => {
+  init();
+
+  const cart = {
+    p92779: {
+      name: "Футболка Теория относительности и коты, серая",
+      url: "#",
+      image: "./img/cart/p1.jpg",
+      price: 1720.0,
+      count: 1,
+    },
+    p93039: {
+      name: "Плакат Воздушный шар",
+      url: "#",
+      image: "./img/cart/p2.jpg",
+      price: 872.0,
+      count: 1,
+    },
+    p93034: {
+      name: "Блокнот TOP SECRET, A5",
+      url: "#",
+      image: "./img/cart/p3.jpg",
+      price: 672.0,
+      count: 1,
+    },
+    p93035: {
+      name: "Услуга подарочной упаковки",
+      url: "#",
+      image: "./img/cart/p4.jpg",
+      price: 2500.0,
+      count: 1,
+    },
+  };
+
+  let shopCart = new Cart(cart);
+  document.querySelector(".cart__cards").innerHTML = "";
+  document.querySelector(".cart__cards").innerHTML = shopCart.render();
+  projectFunc.initSelect(".js-select");
+  projectFunc.initElect();
+
+  document.querySelector(".cart__cards").addEventListener("click", (event) => {
+    let target = event.target;
+
+    if (target.classList.contains("delete")) {
+      console.log(1);
+      shopCart.goodsDelete(target.dataset["articul"]);
+      document.querySelector(".cart__cards").innerHTML = "";
+      document.querySelector(".cart__cards").innerHTML = shopCart.render();
+      projectFunc.initSelect(".js-select");
+      projectFunc.initElect();
+      return true;
+    } else if (target.classList.contains("js-plus")) {
+      event.preventDefault();
+      shopCart.goodsPlus(target.dataset["articul"]);
+      document.querySelector(".cart__cards").innerHTML = "";
+      document.querySelector(".cart__cards").innerHTML = shopCart.render();
+      projectFunc.initSelect(".js-select");
+      projectFunc.initElect();
+      return true;
+    } else if (target.classList.contains("js-minus")) {
+      shopCart.goodsMinus(target.dataset["articul"]);
+      document.querySelector(".cart__cards").innerHTML = "";
+      document.querySelector(".cart__cards").innerHTML = shopCart.render();
+      projectFunc.initSelect(".js-select");
+      projectFunc.initElect();
+      return true;
+    }
+  });
+});
